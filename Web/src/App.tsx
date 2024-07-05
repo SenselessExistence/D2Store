308d15b83fed0c654abff1d69ed9aa06721984a9
Vlad <Vlad@DESKTOP-5LTT00L>
2024-07-05 14:03:14 +0300

    localization

--- Web/src/App.tsx
+++ Web/src/App.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
 import { Outlet } from 'react-router-dom';
+import "./i18n";
 
 function App() {
+  const { i18n } = useTranslation();
+
+  useEffect(()=>{
+    i18n.changeLanguage(navigator.language.split('-')[0]); 
+  },[i18n]);
+  
   return (
     <Outlet/>
   );
--- Web/src/components/navbar/navbar.tsx
+++ Web/src/components/navbar/navbar.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './navbar.scss';
+import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
 function Navbar() {
+  const {t} = useTranslation();
   return (
     <nav>
-      <div>Logo</div>
-      <div>Sign in</div>
+      <div>
+        <img src='/images/logo.png' className='logo' alt='logo'/>
+      </div>
+      <div className='nav-actions'>
+        <Link to='registration'>{t('register')}</Link> |
+        <Link to='login'>{t('login')}</Link>
+      </div>
     </nav>
   );
 }
